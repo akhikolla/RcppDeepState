98ad49b7de81fe09f6019d1502c96bc0c550611e
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-14 15:44:38 -0700

    test compile deepstate2

--- tests/testthat/test-compile-run.R
+++ tests/testthat/test-compile-run.R
@@ -2,7 +2,10 @@ library(testthat)
 context("deepstate_compile_run")
 library(RcppDeepState)
 
-dhc<-deep_harness_compile_run(system.file("testpkgs/testUBSAN", package = "RcppDeepState"))
+
+path <- system.file("testpkgs/testUBSAN", package = "RcppDeepState")
+print(path)
+dhc<-deep_harness_compile_run(path)
 #here::here("/home/travis/build/akhikolla/RcppDeepState/inst/include/testpkgs/binsegRcpp"))
 test_that("compile and run check", {
   expect_match(dhc,"code compiled")
