a877e376ce0f07f85ab3774a83e0e939a49cd8f7
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-16 13:19:52 -0700

    Testing valgrind

--- R/analyze_binary.R
+++ R/analyze_binary.R
@@ -5,17 +5,13 @@
 ##' @return returns a list of all the param values of the arguments of function
 ##' @export
 deep_harness_analyze_one <- function(packagename,function_name,binary_file){
-  function_name <- "rcpp_binseg_normal"
   fun_name <-gsub("rcpp_","",function_name)
   exec.path <- system.file("RcppDeepStatefiles/binsegRcpp", package= "RcppDeepState")
   exec <- paste0("./",fun_name,"_DeepState_TestHarness")
-  
-  binary_file="/home/akhila/R/RcppDeepState/inst/RcppDeepStatefiles/binsegRcpp/binseg_normal_output/dd5b1543eccdc54b284c00142df7f40c1583ac68.crash"
+  binary_file=system.file("RcppDeepStatefiles/binsegRcpp/binseg_normal_output/dd5b1543eccdc54b284c00142df7f40c1583ac68.crash",package="RcppDeepState")
   analyze_one <- paste0(";",exec," --input_test_file ",binary_file)
-  var <- 
-  system()
-  system(analyze_one)
-  system("cd ..")
+  var <- paste("cd",exec.path,analyze_one) 
+  system(var)
   functions.list  <- get_function_body(packagename)
   counter = 1
   #function_name = "rcpp_read_out_of_bound"
@@ -27,7 +23,7 @@ deep_harness_analyze_one <- function(packagename,function_name,binary_file){
       for(argument.i in 1:nrow(functions.rows)){
         arg.name = gsub(" ","",paste0(functions.rows[argument.i,argument.name]))
         #print(arg.name)
-        filepath=paste0("~/R/RcppDeepState/inst/include","/",arg.name)
+        filepath=paste0(system.file("RcppDeepStatefiles/binsegRcpp",package = "RcppDeepState"),"/",arg.name)
         cat("\n", file = filepath, append = TRUE)
         values = read.table(filepath,sep="\t")
         #names(arguments.list) = c(arg.name)
--- tests/testthat/test-names-list-check.R
+++ tests/testthat/test-names-list-check.R
@@ -2,7 +2,7 @@ library(testthat)
 context("deepstate_analyze_one")
 library(RcppDeepState)
 path <- system.file("testpkgs/binsegRcpp", package = "RcppDeepState")
-deep_harness_analyze_one(path,"rcpp_binseg_normal",system.file("RcppDeepStatefiles/binsegRcpp/binseg_normal_output/dd5b1543eccdc54b284c00142df7f40c1583ac68.crash", package = "RcppDeepState"))                         
+arguments.list <- deep_harness_analyze_one(path,"rcpp_binseg_normal",system.file("RcppDeepStatefiles/binsegRcpp/binseg_normal_output/dd5b1543eccdc54b284c00142df7f40c1583ac68.crash", package = "RcppDeepState"))                         
 test_that("argumentlist names validation", {
   expect_identical(names(arguments.list), c("data_vec", "max_segments"))
 })
