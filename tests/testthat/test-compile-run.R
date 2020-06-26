b76738b5571afce264d443efc5d1d702ef69f3fa
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-25 23:01:53 -0700

    deepstate package check tests

--- tests/testthat/test-compile-run.R
+++ tests/testthat/test-compile-run.R
@@ -42,20 +42,9 @@ test_that("check for harness files existence testSAN package", {
 path <- system.file("testpkgs/testSAN", package = "RcppDeepState")
 dhc<-deep_harness_compile_run(path)
 logfiles<-list_log_files(path)
-test_that("check for log files existence testSAN package", {
-  expect_true(file.exists(logfiles[[1]]))
-  expect_identical(logfiles[[1]],system.file("testfiles/testSAN/read_out_of_bound_log",package = "RcppDeepState"))
-  expect_true(file.exists(logfiles[[2]]))
-  expect_identical(logfiles[[2]],system.file("testfiles/testSAN/use_after_deallocate_log",package = "RcppDeepState"))
-  expect_true(file.exists(logfiles[[3]]))
-  expect_identical(logfiles[[3]],system.file("testfiles/testSAN/use_after_free_log",package = "RcppDeepState"))
-  expect_true(file.exists(logfiles[[4]]))
-  expect_identical(logfiles[[4]],system.file("testfiles/testSAN/write_index_outofbound_log",package = "RcppDeepState"))
-  expect_true(file.exists(logfiles[[5]]))
-  expect_identical(logfiles[[5]],system.file("testfiles/testSAN/zero_sized_array_log",package = "RcppDeepState"))
-})
 
-log_path <- system.file("testfiles/testSAN/read_out_of_bound_log", package = "RcppDeepState")
+
+log_path <- system.file("extdata/read_out_of_bound_log", package = "RcppDeepState")
 print(log_path)
 user.display <- user_error_display(log_path)
 test_that("valgrind errors", {
@@ -109,24 +98,6 @@ test_that("valgrind use after free check", {
 })
 
 
-bin_dir<- list_bin_directory(path)
-test_that("check for binary file directories existence testSAN package", {
-  expect_true(dir.exists(bin_dir[[1]]))
-  expect_identical(bin_dir[[1]],system.file("testfiles/testSAN/read_out_of_bound_output",package = "RcppDeepState"))
-  expect_true(dir.exists(bin_dir[[2]]))
-  expect_identical(bin_dir[[2]],system.file("testfiles/testSAN/use_after_deallocate_output",package = "RcppDeepState"))
-  expect_true(dir.exists(bin_dir[[3]]))
-  expect_identical(bin_dir[[3]],system.file("testfiles/testSAN/use_after_free_output",package = "RcppDeepState"))
-  expect_true(dir.exists(bin_dir[[4]]))
-  expect_identical(bin_dir[[4]],system.file("testfiles/testSAN/write_index_outofbound_output",package = "RcppDeepState"))
-  expect_true(dir.exists(bin_dir[[5]]))
-  expect_identical(bin_dir[[5]],system.file("testfiles/testSAN/zero_sized_array_output",package = "RcppDeepState"))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[1]], "*.fail")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[2]], "*.fail")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[3]], "*.fail")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[4]], "*.fail")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[5]], "*.fail")))))
-   })
 
 path <- system.file("testpkgs/binsegRcpp", package = "RcppDeepState")
 res<-deepstate_pkg_create(path)
@@ -153,44 +124,3 @@ test_that("check for make files existence binsegRcpp package", {
 
 path <- system.file("testpkgs/binsegRcpp", package = "RcppDeepState")
 dhc<-deep_harness_compile_run(path)
-logfiles<-list_log_files(path)
-test_that("check for log files existence binsegRcpp package", {
-  expect_true(file.exists(logfiles[[1]]))
-  expect_identical(logfiles[[1]],system.file("testfiles/binsegRcpp/binseg_normal_log",package = "RcppDeepState"))
-  expect_true(file.exists(logfiles[[2]]))
-  expect_identical(logfiles[[2]],system.file("testfiles/binsegRcpp/binseg_normal_cost_log",package = "RcppDeepState"))
-})
-bin_dir<- list_bin_directory(path)
-test_that("check for binary file directories existence binsegRcpp package", {
-  expect_true(dir.exists(bin_dir[[1]]))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[1]], "*.crash")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[1]], "*.fail")))))
-  expect_identical(bin_dir[[1]],system.file("testfiles/binsegRcpp/binseg_normal_output",package = "RcppDeepState"))
-  expect_true(dir.exists(bin_dir[[2]]))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[2]], "*.crash")))))
-  expect_true(all(file.exists(Sys.glob(file.path(bin_dir[[2]], "*.fail")))))
-  expect_identical(bin_dir[[2]],system.file("testfiles/binsegRcpp/binseg_normal_cost_output",package = "RcppDeepState"))
-  
-})
-
-path <- system.file("testpkgs/testSAN",package="RcppDeepState")
-print(path)
-list.args <- list_package_args(path)
-test_that("check for input files testSAN", {
-  expect_true(file.exists(list.args[[1]]))
-  expect_true(file.exists(list.args[[2]]))
-  expect_true(file.exists(list.args[[3]]))
-  expect_true(file.exists(list.args[[4]]))
-  expect_true(file.exists(list.args[[5]]))
-})
-
-bin_file <- Sys.glob(file.path(bin_dir[[1]], "*.fail"))
-path <- paste0(bin_file[1])
-print(path)
-if(path != "NA"){
-  arguments.list<-deep_harness_analyze_one(path)
-  test_that("argumentlist names validation", {
-    expect_identical(names(arguments.list), c("data_vec", "max_segments"))
-  })
-}
-
