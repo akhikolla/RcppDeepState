7e545d8e0557ec8c18a3209bd421f5f943a117ae
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-29 22:15:37 -0700

    static lib function

--- R/deepstate_static_lib.R
+++ R/deepstate_static_lib.R
@@ -1,16 +1,19 @@
 ##' @title  creates static library
 ##' @export
 deepstate_create_static_lib <- function(){
-  #inst_path <- system.file(package="RcppDeepState")
-  inst_path <- "/home/travis/build/akhikolla/RcppDeepState/inst"
+  inst_path <- system.file(package="RcppDeepState")
   print(inst_path)
-  deepstate_path <- file.path(inst_path,"include")
+  inst_folder <- "/home/travis/build/akhikolla/RcppDeepState/inst"
+  print(inst_folder)
+  deepstate_path <- file.path(inst_folder,"include")
+  #deepstate_path <- file.path(inst_path,"include")
   compile_line <- "gcc -fpic -g -O2 "
   DeepState.c.o <- paste0(compile_line,file.path(deepstate_path,"DeepState.c")," -o ",file.path(deepstate_path,"DeepState.o")," -c ")
   Log.c.o <- paste0(compile_line,file.path(deepstate_path,"Log.c")," -o ",file.path(deepstate_path,"Log.o")," -c ")
   Option.c.o <- paste0(compile_line,file.path(deepstate_path,"Option.c")," -o ",file.path(deepstate_path,"Option.o")," -c ")
   Stream.c.o <- paste0(compile_line,file.path(deepstate_path,"Stream.c")," -o ",file.path(deepstate_path,"Stream.o")," -c ")
   objects <- paste0(file.path(deepstate_path,"DeepState.o")," ",file.path(deepstate_path,"Log.o")," ",file.path(deepstate_path,"Option.o")," ",file.path(deepstate_path,"Stream.o"))
+  print(DeepState.c.o)
   system(DeepState.c.o)
   system(Log.c.o)
   system(Option.c.o)
