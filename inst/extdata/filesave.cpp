044ea6efe5d741b417ffa19b9e58af80b7ef30f6
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-10-01 19:43:22 -0700

    remove serialize files

--- inst/extdata/filesave.cpp
+++
@@ -1,9 +0,0 @@
-// [[Rcpp::depends(qs)]]
-#include <Rcpp.h>
-#include <qs.h>
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-void test(Rcpp::NumericVector nv) {
-  qs::c_qsave(nv, "~/Music/myfiles.qs", "high", "zstd", 1, 15, true, 1);
-}
