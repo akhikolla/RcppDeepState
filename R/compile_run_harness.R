a69574c7b943773566a04b0b1d2b7a70161133d3
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-10-18 23:05:34 -0700

    travis check on

--- R/compile_run_harness.R
+++ R/compile_run_harness.R
@@ -13,7 +13,8 @@ deepstate_harness_compile_run <- function(package_name){
   #print(testharness)
   functions.list <- Sys.glob(file.path(test_path,"*"))
   #print(basename(functions.list))
-  if(testharness == basename(functions.list) && length(functions.list) >= 1){
+  if(testharness == basename(functions.list)){
+    if(length(functions.list) >= 1){
     uncompiled_count = 0
     log_count = 0
     for(fun.path in functions.list){
@@ -27,7 +28,8 @@ deepstate_harness_compile_run <- function(package_name){
           }
     }
     return(as.character(compiled.code))
-  }
+    }
+}
   else{
     print("failed")
   }
