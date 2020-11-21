74354ef383a4b2f1131360e74d2ecddd18c451ad
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-11-21 13:59:51 -0700

    generation and checks added

--- R/editable_fun.R
+++ R/editable_fun.R
@@ -3,5 +3,6 @@
 ##' @param function_name of the package
 ##' @export
 deepstate_editable_fun<-function(package_name,function_name){
-  deepstate_fun_create(package_name,function_name,sep="outfun")  
+  deepstate_fun_create(package_name,function_name,sep="generation")  
+  deepstate_fun_create(package_name,function_name,sep="checks")  
 }
